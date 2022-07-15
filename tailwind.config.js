bfd92342d6ba78b935607e68f3982eae71cd7d1c
yoni1342 <yonatantesfaye30@gmail.com>
2022-07-15 06:57:02 -0700

    style: integrating tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.{js, jsx,css}'],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants:{
+    extend: {},
+  },
+  plugins: [],
+}
