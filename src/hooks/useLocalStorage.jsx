ed11d91de7d5213990f31832be05ec0ac88ca633
yoni1342 <yonatantesfaye30@gmail.com>
2022-07-15 06:36:50 -0700

    refactor: implementing folder stucter

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,24 @@
+// This custom hook is use for storing data in local storage
+
+// importing react hooks
+import { useEffect, useState } from 'react';
+
+function getSavedValue(key, initialValue) {
+	const savedValue = JSON.parse(localStorage.getItem(key));
+	if (savedValue) return savedValue;
+
+	if (initialValue instanceof Function) return initialValue();
+	return initialValue;
+}
+
+export default function useLocalStorage(key, initialValue) {
+	const [value, setValue] = useState(() => {
+		return getSavedValue(key, initialValue);
+	});
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [value]); // eslint-disable-line react-hooks/exhaustive-deps
+
+	return [value, setValue];
+}
